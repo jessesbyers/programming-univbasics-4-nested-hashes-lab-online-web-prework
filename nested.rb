b6f3c19ea9fd57ee6b90f0f41bf3529772f1bebe
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-30 18:31:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
      programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
     :languages => ["LISP", "C"]}
     programmer_hash
@@ -105,6 +104,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
 end
 
