5d16920600ae22b153b873b512257cb086000d4b
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-30 18:33:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
      programmer_hash
-     
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -126,4 +126,5 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages]
 end
