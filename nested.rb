d35bcaa043b7ca98ea80572647bd627aa21df041
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-30 18:21:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,12 @@ end
 
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
+#:yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
+programmer_hash
 
 	programmer_hash = 
  		{
@@ -88,8 +89,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
